36a21ed57357fa48c284ae9b356160d0c4c9a814
[ReCreateIndia] <18ucs066@lnmiit.ac.in>
2020-08-10 12:32:01 +0530

    Updates

---
+++ precache-manifest.7fb9d293a0de1573e3d98b4939ba3ea3.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33b7e88bdd2f207c414d279eab8608f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e917123bad64c50399b1",
+    "url": "/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "781c625339aa8ab55b1b",
+    "url": "/static/css/main.960c9096.chunk.css"
+  },
+  {
+    "revision": "e917123bad64c50399b1",
+    "url": "/static/js/2.f381903d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f381903d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "781c625339aa8ab55b1b",
+    "url": "/static/js/main.51b9608b.chunk.js"
+  },
+  {
+    "revision": "96ba2b548d0dadf61791",
+    "url": "/static/js/runtime-main.5519957e.js"
+  },
+  {
+    "revision": "e9cbf7efdba90859dd8d1b638141b556",
+    "url": "/static/media/backgroundwebpage.e9cbf7ef.jpg"
+  },
+  {
+    "revision": "72d8b30c08ca6149a5fa88b45496c577",
+    "url": "/static/media/happyimage1.72d8b30c.jpg"
+  },
+  {
+    "revision": "54df368381ab7a6e604479af91b1c814",
+    "url": "/static/media/happyimage10.54df3683.jpg"
+  },
+  {
+    "revision": "02ae22eb924f9eed40575845c02f9967",
+    "url": "/static/media/happyimage11.02ae22eb.jpg"
+  },
+  {
+    "revision": "9d52a4d9278c27d91aa5f622b0cbc784",
+    "url": "/static/media/happyimage2.9d52a4d9.jpg"
+  },
+  {
+    "revision": "98f2d9b373818a6a5bad3e82c03b0b8c",
+    "url": "/static/media/happyimage3.98f2d9b3.jpg"
+  },
+  {
+    "revision": "3ebad266a113eec59b2a1fca11a5eca2",
+    "url": "/static/media/happyimage4.3ebad266.jpg"
+  },
+  {
+    "revision": "1d5cc4abe795604ff0fb60a8fd56f04e",
+    "url": "/static/media/happyimage5.1d5cc4ab.jpg"
+  },
+  {
+    "revision": "9bd9ab71612d6cccaf303047f91de6cc",
+    "url": "/static/media/happyimage6.9bd9ab71.jpg"
+  },
+  {
+    "revision": "62056620c78b17e43e3666958aa6fa62",
+    "url": "/static/media/happyimage7.62056620.jpg"
+  },
+  {
+    "revision": "60fa2b86355a30f75f3f98f34a47d16a",
+    "url": "/static/media/happyimage8.60fa2b86.jpg"
+  },
+  {
+    "revision": "2ad98d2a615a3864e122bfb4aa13c3e6",
+    "url": "/static/media/happyimage9.2ad98d2a.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
